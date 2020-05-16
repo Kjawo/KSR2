55bb0f718b17f20540df299beaf75d03d0806ed6
Kjawo <konjaworski@gmail.com>
2020-05-16 18:04:19 +0200

    Update LinguisticVariable with multiple labels/functions

--- src/main/java/edu/tul/ksr2/LinguisticVariable/LinguisticVariable.java
+++ src/main/java/edu/tul/ksr2/LinguisticVariable/LinguisticVariable.java
@@ -1,49 +1,25 @@
 package edu.tul.ksr2.LinguisticVariable;
 
-import edu.tul.ksr2.GameEntity;
-import org.xml.sax.SAXException;
+import edu.tul.ksr2.MembershipFunctions.MembershipFunction;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
-import org.w3c.dom.Document;
 
 public class LinguisticVariable {
 
-    double a;
-    double b;
-    double c;
-    double d;
-    String tag;
+    ArrayList<String> labels;
+    ArrayList<MembershipFunction> membershipFunctions;
 
-    public LinguisticVariable(String tag) {
-        this.tag = tag;
+    public LinguisticVariable(ArrayList<String> labels, ArrayList<MembershipFunction> membershipFunctions) {
+        this.labels = labels;
+        this.membershipFunctions = membershipFunctions;
     }
 
-    public void loadData(ArrayList<GameEntity> gameEntities){
-        ArrayList<Object> arrayList = (ArrayList<Object>) gameEntities.stream()
-                                        .map(gameEntity -> gameEntity.get(tag))
-                                        .collect(Collectors.toList());
-//        System.out.println(arrayList);
-
-        File file = new File("src\\main\\resources\\data\\MembershipParameters.xml");
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-                .newInstance();
-        DocumentBuilder documentBuilder;
-        Document document = null;
-        try {
-            documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            document = documentBuilder.parse(file);
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-        if (document != null) {
-            System.out.println(document.getElementById("Duration").getTextContent());
-        }
-
+    @Override
+    public String toString() {
+        return "LinguisticVariable{" +
+                "labels=" + labels +
+                ", membershipFunctions=" + membershipFunctions +
+                '}';
     }
+
 }

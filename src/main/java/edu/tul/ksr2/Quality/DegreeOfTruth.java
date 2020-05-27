af017157df6dfd711d356a5dd8c117910a093413
Kjawo <konjaworski@gmail.com>
2020-05-27 13:03:28 +0200

    Add quality skeleton

---
+++ src/main/java/edu/tul/ksr2/Quality/DegreeOfTruth.java
@@ -0,0 +1,13 @@
+package edu.tul.ksr2.Quality;
+
+import edu.tul.ksr2.GameEntity;
+import edu.tul.ksr2.LinguisticVariable.Quantifier;
+import edu.tul.ksr2.LinguisticVariable.Summarizer;
+
+import java.util.ArrayList;
+
+public class DegreeOfTruth {
+    public static Double compute(Quantifier quantifier, Summarizer summarizer, ArrayList<GameEntity> gameEntities) {
+        return null;
+    }
+}

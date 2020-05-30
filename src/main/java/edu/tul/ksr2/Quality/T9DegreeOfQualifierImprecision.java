395d5aeca9e7224b9babdbf72edb6494d1edb012
Skyblade <piotr8715@gmail.com>
2020-05-30 13:42:29 +0200

    NullPointer fix in T9 when there is no qualifier chosen

--- src/main/java/edu/tul/ksr2/Quality/T9DegreeOfQualifierImprecision.java
+++ src/main/java/edu/tul/ksr2/Quality/T9DegreeOfQualifierImprecision.java
@@ -12,9 +12,10 @@ public class T9DegreeOfQualifierImprecision {
     public static Double computeFirstType(Quantifier quantifier, ArrayList<Summarizer> summarizers, ArrayList<GameEntity> gameEntities, Summarizer qualifier) {
 
         Double T9 = 1.0;
-//        if(qualifier.)
-        T9 *= qualifier.getFuzzySet().calculateDegreeOfFuzziness();
-        T9 = 1.0 - Math.pow(T9, 1.0);
+        if(qualifier != null) {
+            T9 *= qualifier.getFuzzySet().calculateDegreeOfFuzziness();
+            T9 = 1.0 - Math.pow(T9, 1.0);
+        }
         return T9;
     }
 }
